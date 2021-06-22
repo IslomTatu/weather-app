1e9f548b07b4ccbc079215f2c91893fa04d30599
IslomQurbonov <islom_uz@inbox.ru>
2021-06-22 13:47:46 +0500

    changed for working correctly

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,15 @@
+/* api key for weather api
+  added for bug free and trouble-free operation
+  should be located in .env file
+*/
+const apiKey = '7387304fef0358b40e1a3f0ce27a72bf'
+
 export default {
   getList: ({ lat, lon }) =>
-  `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely&appid=${process.env.API_KEY}`,
+  `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely&appid=${process.env.API_KEY || apiKey}`,
 
   getHourlyList: ({ lat, lon }) =>
-    `http://api.openweathermap.org/data/2.5/forecast?&units=metric&lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}`,
+    `http://api.openweathermap.org/data/2.5/forecast?&units=metric&lat=${lat}&lon=${lon}&appid=${process.env.API_KEY || apiKey}`,
 
   iconUrl: icon => `http://openweathermap.org/img/wn/${icon}@2x.png`
 }
